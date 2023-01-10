7192377ffae9d2cd950d8d735f0afed586bffed1
Nick Sollon <genericjoke@gmail.com>
2023-01-10 09:06:34 -0500

    commit tree 47b07bcd4e825c2f20e375f763955721dfa4a588

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (divers) => ([divers[0], divers[1]]);
+const returnLastTwoDrivers = (divers) => ([divers[divers.length - 2], divers[divers.length - 1]]);
+const selectingDrivers = 0;
